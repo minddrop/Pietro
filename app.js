e34e3caed7c5ee799753a82f21d9b2c3831782dc
minddrop <ryooooooo1@gmail.com>
2019-05-24 13:14:20 +0900

    run prettier

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express')
+const morgan = require('morgan')
 
-const app = express();
-app.use(morgan("combined"));
+const app = express()
+app.use(morgan('combined'))
 
-app.get("/hello", (req, res) => {
-  res.send("hello express");
-});
-app.get("/", express.static("dist"));
-app.get("/oauth", (req, res) => {
-  res.send("oauth");
-});
-const port = process.env.PORT;
+app.get('/hello', (req, res) => {
+  res.send('hello express')
+})
+app.get('/', express.static('dist'))
+app.get('/oauth', (req, res) => {
+  res.send('oauth')
+})
+const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`Starting server listening to ${port}`);
-});
+  console.log(`Starting server listening to ${port}`)
+})
