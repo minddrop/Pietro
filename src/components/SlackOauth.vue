acfef65e5a4ce12cd433baba61fbd24a2852c83b
minddrop <ryooooooo1@gmail.com>
2019-05-27 19:52:39 +0900

    update slack-oauth page

--- src/components/SlackOauth.vue
+++ src/components/SlackOauth.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container">
     <p>slack-oauth page</p>
+    <p>Give permission to message as you.</p>
+
+    <a :href="'https://slack.com/oauth/authorize?scope='+ scope +'&client_id='+ clientId">
+      <img
+        alt="Add to Slack"
+        height="40"
+        width="139"
+        src="https://platform.slack-edge.com/img/add_to_slack.png"
+        srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+      >
+    </a>
+    <input v-model="clientId" placeholder="Enter client_id">
+    <div>client_id is {{ clientId ? clientId : '...' }}</div>
+    <div>{{'scope is '+ scope}}</div>
     <router-link to="/">
       <a>go home</a>
     </router-link>
@@ -9,7 +23,13 @@
 
 <script>
 export default {
-  name: 'SlackOauth'
+  name: 'SlackOauth',
+  data() {
+    return {
+      clientId: '',
+      scope: 'chat:write:user'
+    }
+  }
 }
 </script>
 
