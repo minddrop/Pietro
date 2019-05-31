e6e204495401053c28538c46f319957a6f70fe3b
minddrop <ryooooooo1@gmail.com>
2019-05-31 22:10:27 +0900

    introduced body-parser middleware

--- src/server/app.js
+++ src/server/app.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const morgan = require('morgan')
 const apiFallback = require('connect-history-api-fallback')
+const bodyParser = require('body-parser')
 
 const app = express()
-app.use(morgan('combined'))
+app.use(morgan('short'))
+app.use(bodyParser.json())
 app.use(express.static('dist'))
-app.use(apiFallback({ disableDotRule: true, verbose: true }))
 
 app.get('/hello', (req, res) => {
   res.send('hello express')
 })
-app.get('/index.html', express.static('dist'))
 app.get('/oauth', (req, res) => {
   res.send('oauth')
 })
+app.post('/test', (req, res) => {
+  res.send(req.body)
+})
+app.use(apiFallback({ disableDotRule: true, verbose: true }))
+app.get('/index.html', express.static('dist'))
+
 const port = process.env.PORT_SERVER
 app.listen(port, () => {
   console.log(`Starting server listening to ${port}`)
