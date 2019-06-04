46ec6bc36f2aeaf745d623ae27defa9c640c3483
minddrop <ryooooooo1@gmail.com>
2019-06-04 18:44:00 +0900

    error handling

--- src/server/app.js
+++ src/server/app.js
@@ -8,19 +8,47 @@ const app = express()
 app.use(cors())
 app.use(morgan('short'))
 app.use(bodyParser.json())
+
+// public
 app.use(express.static('dist'))
 
-app.get('/hello', (req, res) => {
-  res.send('hello express')
-})
-app.get('/oauth', (req, res) => {
-  res.send('oauth')
-})
-app.post('/test', (req, res) => {
-  res.send(req.body)
-})
+const middleWrapper = f => (req, res, next) => {
+  return new Promise(resolve => {
+    resolve(f(req, res, next))
+  }).catch(next)
+}
+
+// api
+app.get(
+  '/hello',
+  middleWrapper((req, res) => {
+    res.send('hello express')
+  })
+)
+app.get(
+  '/oauth',
+  middleWrapper((req, res) => {
+    res.send('oauth')
+  })
+)
+app.post(
+  '/test',
+  middleWrapper((req, res) => {
+    res.send(req.body)
+  })
+)
+
+// fallback
 app.use(apiFallback({ disableDotRule: true, verbose: true }))
+
+// vue
 app.get('/index.html', express.static('dist'))
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 const port = process.env.PORT_SERVER
 app.listen(port, () => {
