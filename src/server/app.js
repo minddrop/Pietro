e7bf0f513669abb7029695b241d200f1f551e62f
minddrop <ryooooooo1@gmail.com>
2019-06-15 14:42:57 +0900

    used slack web api to post mmessage

--- src/server/app.js
+++ src/server/app.js
@@ -3,16 +3,38 @@ const morgan = require('morgan')
 const apiFallback = require('connect-history-api-fallback')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const crypto = require('crypto')
+const { WebClient } = require('@slack/web-api')
+const rp = require('request-promise-native')
+const methods = require('./methods.json')
 
+const {
+  CLIENT_ID,
+  CLIENT_SECRET,
+  PORT_SERVER,
+  PASSWORD,
+  SALT,
+  CRYPTO_ALGORITHM,
+  REDIRECT_URI
+} = process.env
+const port = PORT_SERVER
+const key = crypto.scryptSync(PASSWORD, SALT, 32)
+if (!PASSWORD || !SALT) {
+  throw new Error('PASSWORD or SALT or both are not found.')
+}
+if (!CRYPTO_ALGORITHM) {
+  throw new Error('No algorithm specified.')
+}
 const app = express()
 app.use(cors())
 app.use(morgan('short'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // public
 app.use(express.static('dist'))
 
-const middleWrapper = f => (req, res, next) => {
+const middlewareWrapper = f => (req, res, next) => {
   return new Promise(resolve => {
     resolve(f(req, res, next))
   }).catch(next)
@@ -21,19 +43,75 @@ const middleWrapper = f => (req, res, next) => {
 // api
 app.get(
   '/hello',
-  middleWrapper((req, res) => {
+  middlewareWrapper((req, res) => {
     res.send('hello express')
   })
 )
 app.get(
   '/oauth',
-  middleWrapper((req, res) => {
-    res.send('oauth')
+  middlewareWrapper((req, res) => {
+    const { code, state } = req.query
+    const options = {
+      uri: 'https://slack.com/api/oauth.access',
+      qs: {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        code: code,
+        redirect_uri: REDIRECT_URI
+      },
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    }
+    rp(options)
+      .then(repos => {
+        const { access_token } = repos
+        res.header('Content-Type', 'text/plain; charset=utf-8')
+        const r = encrypt(access_token)
+        res.send(
+          `次のフレーズをコピーしてください\n\n${JSON.stringify(
+            encrypt(access_token)
+          )}`
+        )
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send()
+      })
+  })
+)
+app.post(
+  '/',
+  middlewareWrapper((req, res) => {
+    const { cryptedToken, iv, method, options } = req.body
+    const access_token = decrypt(cryptedToken, Buffer.from(iv, 'base64'))
+    const web = new WebClient(access_token)
+
+    const methodScopes = method.split('.')
+    if (
+      methodScopes[0] !== 'web' ||
+      methods.includes(methodScopes.slice(1).join(''))
+    ) {
+      res.status(500).json()
+    }
+    methodScopes[0] = web
+    const webAPIMethod = methodScopes.reduce(
+      (previous, current) => previous[current]
+    )
+
+    webAPIMethod(options)
+      .then(response => {
+        res.json(response)
+      })
+      .catch(err => {
+        res.status(500).json(err.data || err.message)
+      })
   })
 )
 app.post(
   '/test',
-  middleWrapper((req, res) => {
+  middlewareWrapper((req, res) => {
     res.send(req.body)
   })
 )
@@ -50,7 +128,22 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!')
 })
 
-const port = process.env.PORT_SERVER
 app.listen(port, () => {
   console.log(`Starting server listening to ${port}`)
 })
+
+const decrypt = (cryptedToken, iv) => {
+  const decipher = crypto.createDecipheriv(CRYPTO_ALGORITHM, key, iv)
+  const r =
+    decipher.update(cryptedToken, 'base64', 'utf-8') + decipher.final('utf-8')
+  return r
+}
+
+const encrypt = accessToken => {
+  const iv = crypto.randomBytes(16)
+  const cipher = crypto.createCipheriv(CRYPTO_ALGORITHM, key, iv)
+  const crypted =
+    cipher.update(accessToken, 'utf-8', 'base64') + cipher.final('base64')
+  const r = { cryptedToken: crypted, iv: iv.toString('base64') }
+  return r
+}
